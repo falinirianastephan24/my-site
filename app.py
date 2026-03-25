92f374078e02ab2a39cab23f4181b2e1b01d0c84
my-site <falinirianastephan@gmail.com>
2026-03-25 08:32:33 +0100

    Fix: Set host to 0.0.0.0 and port for Render

--- app.py
+++ app.py
@@ -36,6 +36,15 @@ if __name__ == '__main__':
     init_db()
     current_dir = os.path.dirname(os.path.abspath(__file__))
     
+    # 1. Raiso ny Port omen'ny Render (default 10000 raha tsy misy)
+    port = int(os.environ.get("PORT", 10000))
+    
+    # 2. Ampidiro ny config ho an'ny Server (Host 0.0.0.0 no ilain'ny Render)
+    cherrypy.config.update({
+        'server.socket_host': '0.0.0.0',
+        'server.socket_port': port,
+    })
+    
     config = {
         '/css_sy_sary': {
             'tools.staticdir.on': True,
